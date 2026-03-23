a6f26b323c3389e7fe15be8f60eaa125a0d7a7dd
akshayakuthuru <akshayakuthuru@gmail.com>
2026-03-23 16:33:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function display(value){
+    document.getElementById("result").value += value;
+}
+
+function calculate(){
+    let x = document.getElementById("result").value;
+    let y = eval(x);
+    document.getElementById("result").value = y;
+}
+
+function clearScreen(){
+    document.getElementById("result").value = "";
+}
